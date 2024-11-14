6029bb15363e0d508078b2c2e052c90f9a8b349a
CooDiiNgg <93319862+CooDiiNgg@users.noreply.github.com>
2024-11-15 00:50:55 +0200

    ooops

--- server/app/app.py
+++ server/app/app.py
@@ -16,5 +16,4 @@ def handle_error(e):
     return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    print(os.getenv('FLASK_SECRET_KEY'))
     app.run(host='127.0.0.1', port=5000, debug=False)
