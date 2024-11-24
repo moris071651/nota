93230711567029a7e799c681e7b214884fa2b16c
CooDiiNgg <93319862+CooDiiNgg@users.noreply.github.com>
2024-11-25 00:10:10 +0200

    final session adjustments

--- server/app/app.py
+++ server/app/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 from blueprints.routes import api
+from flask_session import Session
 
 env_path = os.path.join(os.path.dirname(__file__), '../../.env')
 env_path = os.path.abspath(env_path)
@@ -9,6 +10,13 @@ load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
+app.config['SESSION_COOKIE_NAME'] = 'flask_session'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_COOKIE_SECURE'] = False
+app.config['SESSION_PERMANENT'] = True
+
+Session(app)
+
 app.register_blueprint(api, url_prefix='/api')
 
 @app.errorhandler(Exception)
